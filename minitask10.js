083a3af12dae87871906eaa6f5c8fe1b66aab000
ariramadhan <ariboss89@gmail.com>
2025-11-07 16:29:50 +0700

    membuat-fungsi-manual-suatu-method-mencari-nilai-max-array

---
+++ minitask10.js
@@ -0,0 +1,18 @@
+//Method Array (Mencari nilai maksimum)
+let count = 0;
+const arr = [10, 8, 12, 11, 1];
+
+let number = arr[0];
+
+let temp = 0;
+let b = 0;
+
+//mencari maksimum
+for (let a = 0; a < arr.length; a++) {
+  //nomor pertama kan 6
+  if (arr[a] >= number) {
+    number = arr[a];
+  }
+}
+
+console.log("Nilai maksimalnya adalah : ", number);
